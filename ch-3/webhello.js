59bc35a6f935fc3b71dc3d02cf09e6d21605f020
azka-alterra <azka.ramadhan@alterra.id>
2023-05-22 23:41:59 +0700

    chapter 3 - simple web server

---
+++ ch-3/webhello.js
@@ -0,0 +1,21 @@
+const
+    port = (process.argv[2] || process.env.PORT || 3000),
+    http = require('http');
+
+http.createServer((req, res) => {
+    const nameArg = capitalize(req.url.replace(/[^\w.,-]/g, ' ').replace(/\s+/g, ' ').trim() || 'world' );
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-html');
+    res.end(`<p>Hello ${nameArg}</p>`)
+}).listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
+
+function capitalize(str) {
+    return str
+        .trim()
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
